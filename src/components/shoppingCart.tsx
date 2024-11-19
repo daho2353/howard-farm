0b1dcc81f29cadb96ab59d04f497ef64900f65af
daho2353 <daho2353@colorado.edu>
2024-11-18 18:52:35 -0800

    fixed remove from cart function to remove 1 item instead of all

--- src/components/shoppingCart.tsx
+++ src/components/shoppingCart.tsx
@@ -12,7 +12,15 @@ interface Props{
 const ShoppingCart: React.FC<Props> = ({cart, setCart}) => {
     
     const removeFromCart = (products: Product) => {
-        setCart((prevCart: Product[]) =>  prevCart.filter(cartItem => cartItem.name !== products.name));
+            setCart((prevCart: Product[]) => {
+                const index = prevCart.findIndex(cartItem => cartItem.name === products.name);
+                if (index !== -1) {
+                    // Remove the item at the found index
+                    return [...prevCart.slice(0, index), ...prevCart.slice(index + 1)];
+                }
+                return prevCart; // If not found, return the cart unchanged
+            });
+        
     };
 
    return (
